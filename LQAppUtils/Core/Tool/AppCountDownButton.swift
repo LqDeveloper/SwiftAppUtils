5e95863047f6db4257227623b1ceb9f7bc439354
Quan Li <quan.li@williamoneilchina.com>
2019-10-25 14:53:26 +0800

    添加倒计时button

---
+++ LQAppUtils/Core/Tool/AppCountDownButton.swift
@@ -0,0 +1,86 @@
+//
+//  AppCountDownButton.swift
+//  LQAppUtils
+//
+//  Created by Quan Li on 2019/10/25.
+//  Copyright © 2019 williamoneilchina. All rights reserved.
+//
+
+import UIKit
+
+public protocol AppCountDownDelegate:NSObjectProtocol{
+    func clickCountDown(btn:AppCountDownButton)
+    func beginCountDown(btn:AppCountDownButton,count:Int)
+    func countDowning(btn:AppCountDownButton,remainSecond:Int)
+    func countDownComplete(btn:AppCountDownButton)
+}
+
+open class AppCountDownButton: UIButton {
+    public var count:Int = 60{
+        didSet{
+            remainCount = count
+        }
+    }
+    private var remainCount = 60
+    
+    public var countTimer:Timer?
+    
+    public weak var delegate:AppCountDownDelegate?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configBtn()
+    }
+    
+    public convenience init(){
+        self.init(frame: CGRect.zero)
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configBtn()
+    }
+    
+    public func configBtn(){
+         addTarget(self, action: #selector(clickButton), for: .touchUpInside)
+    }
+    
+    @objc func clickButton(){
+        isUserInteractionEnabled = false
+        delegate?.clickCountDown(btn: self)
+    }
+    
+    public func startCountDown(){
+        guard countTimer == nil else {
+            return
+        }
+        countTimer = Timer.init(timeInterval: 1, repeats: true, block: {[weak self] (_) in
+            self?.timerRun()
+        })
+        guard let count = countTimer else {
+            return
+        }
+        delegate?.beginCountDown(btn: self, count: remainCount)
+        RunLoop.current.add(count, forMode: .default)
+    }
+    
+    func timerRun(){
+        if remainCount == 1{
+            stopCountDown()
+            delegate?.countDownComplete(btn: self)
+        }else{
+            remainCount -= 1
+            delegate?.countDowning(btn: self, remainSecond: remainCount)
+        }
+    }
+    
+    public func stopCountDown(){
+        guard countTimer != nil else {
+            return
+        }
+        isUserInteractionEnabled = true
+        remainCount = count
+        countTimer?.invalidate()
+        countTimer = nil
+    }
+}
--- LQAppUtils/ViewController.swift
+++ LQAppUtils/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: AppBaseViewController{
+    lazy var countButton = { () -> AppCountDownButton in
+        let count = AppCountDownButton.init(title: "倒计时", font: UIFont.systemFont(ofSize: 16), normalColor: .red)
+        count.backgroundColor = .green
+        count.frame = CGRect.init(x: 100, y: 100, width: 100, height: 30)
+        count.count = 5
+        count.delegate = self
+        return count
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        view.addSubview(countButton)
     }
  
     func aes(){
@@ -31,3 +39,17 @@ class ViewController: AppBaseViewController{
     }
 }
 
+extension ViewController:AppCountDownDelegate{
+    func clickCountDown(btn: AppCountDownButton) {
+        btn.startCountDown()
+    }
+    func beginCountDown(btn: AppCountDownButton, count: Int) {
+        btn.setTitle("\(count)s", for: .normal)
+    }
+    func countDowning(btn: AppCountDownButton, remainSecond: Int) {
+        btn.setTitle("\(remainSecond)s", for: .normal)
+    }
+    func countDownComplete(btn: AppCountDownButton) {
+        btn.setTitle("倒计时", for: .normal)
+    }
+}
