67d30de3e9e41570ced7e55ee9a01d60cb0aacbd
lq <1083099465@qq.com>
2019-10-27 15:32:45 +0800

    添加MD5加密

--- LQAppUtils/Core/Extension/StringExtensions.swift
+++ LQAppUtils/Core/Extension/StringExtensions.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2019 Quan Li. All rights reserved.
 //
 
-#if canImport(UIKit) && !os(watchOS)
+#if canImport(UIKit) && !os(watchOS) && canImport(CommonCrypto)
 import UIKit
-
+import CommonCrypto
 public extension NSString{
     func size(font:UIFont) -> CGSize {
         let fontAttribute = [NSAttributedString.Key.font:font]
@@ -58,6 +58,22 @@ public extension String {
     /// character 数组
     var charactersArray: [Character] {
         return Array(self)
+    }
+    
+    
+    /// MD5加密
+    func md5() -> String {
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = CUnsignedInt(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
+        CC_MD5(str!, strLen, result)
+        let hash = NSMutableString()
+        for i in 0 ..< digestLen {
+            hash.appendFormat("%02x", result[i])
+        }
+        free(result)
+        return String(format: hash as String)
     }
     
     
