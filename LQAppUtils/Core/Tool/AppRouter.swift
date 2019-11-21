3b4ddf319b2d28885c8e6190dc5599789d0096f1
Quan Li <quan.li@williamoneilchina.com>
2019-11-21 17:00:22 +0800

    1.5.9

--- LQAppUtils/Core/Tool/AppRouter.swift
+++ LQAppUtils/Core/Tool/AppRouter.swift
@@ -15,9 +15,9 @@ public enum PushType{
 }
 
 public enum AppRouterType {
-    case push(_ vc:UIViewController,_ pushType:PushType = .notHideBar)
-    case present(_ vc:UIViewController)
-    case back(_ toRoot:Bool)
+    case push(_ vc:UIViewController,_ pushType:PushType = .notHideBar,_ animated:Bool = true)
+    case present(_ vc:UIViewController,_ animated:Bool = true)
+    case back(_ toRoot:Bool,_ animated:Bool = true)
 }
 
 
@@ -29,35 +29,35 @@ public class AppRouter{
     
     public func switchToViewController(type:AppRouterType){
         switch type {
-        case .push(let vc,let pushType):
+        case .push(let vc,let pushType,let animated):
             switch pushType {
             case .notHideBar:
-                currentVC?.navigationController?.pushViewController(vc)
+                currentVC?.navigationController?.pushViewController(vc, animated: animated)
             case .hideWhenPush:
                 currentVC?.hidesBottomBarWhenPushed = true
-                currentVC?.navigationController?.pushViewController(vc)
+                currentVC?.navigationController?.pushViewController(vc, animated: animated)
             case .hideWhenPushAndBackNotHide:
                 currentVC?.hidesBottomBarWhenPushed = true
-                currentVC?.navigationController?.pushViewController(vc)
+                currentVC?.navigationController?.pushViewController(vc, animated: animated)
                 currentVC?.hidesBottomBarWhenPushed = false
             }
-        case .present(let vc):
-            currentVC?.present(vc, animated: true, completion: nil)
-        case .back(let toRoot):
+        case .present(let vc,let animated):
+            currentVC?.present(vc, animated: animated, completion: nil)
+        case .back(let toRoot,let animated):
             if currentVC?.presentingViewController != nil {
-                currentVC?.dismiss(animated: true, completion: nil)
+                currentVC?.dismiss(animated: animated, completion: nil)
             } else {
                 if toRoot{
-                    _ = currentVC?.navigationController?.popToRootViewController(animated: true)
+                    _ = currentVC?.navigationController?.popToRootViewController(animated: animated)
                 }else{
-                    _ = currentVC?.navigationController?.popViewController(animated: true)
+                    _ = currentVC?.navigationController?.popViewController(animated: animated)
                 }
             }
         }
     }
     
-    public func backToVC(_ toRoot:Bool = false){
-        switchToViewController(type: .back(toRoot))
+    public func backToVC(_ toRoot:Bool = false,_ animated:Bool = true){
+        switchToViewController(type: .back(toRoot,animated))
     }
 }
 
