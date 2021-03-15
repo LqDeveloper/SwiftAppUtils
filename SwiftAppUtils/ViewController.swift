879c0907f5b0d7f3f577e487eca0051a77a2ddc8
Quan Li <quan.li@williamoneilchina.com>
2021-03-15 13:15:17 +0800

    Initial Commit

---
+++ SwiftAppUtils/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAppUtils
+//
+//  Created by Quan Li on 2021/3/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
